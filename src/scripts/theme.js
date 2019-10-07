a0adb488b725ac55f706b59266531b3d394e17b4
zlarsen <zlarsen@dmail.dixie.edu>
2019-10-07 16:26:09 -0700

    Make default theme dark

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,39 +1,39 @@
-;(function() {
-	var listeners = []
-	var next = () => {
-		listeners.forEach(l => l(window.isCurrentThemeDark))
-	}
+(function() {
+  var listeners = [];
+  var next = () => {
+    listeners.forEach(l => l(window.isCurrentThemeDark));
+  };
 
-	var themeQuery = matchMedia('(prefers-color-scheme: dark)')
-	window.isCurrentThemeDark = false
+  var themeQuery = matchMedia('(prefers-color-scheme: dark)');
+  window.isCurrentThemeDark = true;
 
-	window.themeObservable = {
-		subscribe: listener => {
-			listeners.push(listener)
-			return () => listeners.filter(l => l !== listener)
-		},
-	}
+  window.themeObservable = {
+    subscribe: listener => {
+      listeners.push(listener);
+      return () => listeners.filter(l => l !== listener);
+    }
+  };
 
-	function setTheme(isNextThemeDark) {
-		window.isCurrentThemeDark = isNextThemeDark
-		document.body.className = window.isCurrentThemeDark ? 'dark' : 'light'
-		next()
-	}
+  function setTheme(isNextThemeDark) {
+    window.isCurrentThemeDark = isNextThemeDark;
+    document.body.className = window.isCurrentThemeDark ? 'dark' : 'light';
+    next();
+  }
 
-	window.toggleTheme = function() {
-		isNextThemeDark = !window.isCurrentThemeDark
-		setTheme(isNextThemeDark)
-		try {
-			localStorage.setItem('isThemeDark', isNextThemeDark)
-		} catch (err) {}
-	}
+  window.toggleTheme = function() {
+    isNextThemeDark = !window.isCurrentThemeDark;
+    setTheme(isNextThemeDark);
+    try {
+      localStorage.setItem('isThemeDark', isNextThemeDark);
+    } catch (err) {}
+  };
 
-	themeQuery.addListener(function(e) {
-		setTheme(e.matches)
-	})
+  themeQuery.addListener(function(e) {
+    setTheme(e.matches);
+  });
 
-	try {
-		var isStoredThemeDark = localStorage.getItem('isThemeDark') === 'true'
-		setTheme(isStoredThemeDark)
-	} catch (err) {}
-})()
+  try {
+    var isStoredThemeDark = localStorage.getItem('isThemeDark') === 'true';
+    setTheme(isStoredThemeDark);
+  } catch (err) {}
+})();
